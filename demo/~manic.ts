8d4853f7f640062b3888091b466a89c58c63d0b8
Rahul Marban <rahulmarban@gmail.com>
2026-04-16 01:58:35 +0530

    refactor: lint rules

--- demo/~manic.ts
+++ demo/~manic.ts
@@ -1,6 +1,6 @@
 import { createManicServer } from "manicjs/server";
 import app from "./app/index.html";
 
-const server = await createManicServer({ html: app });
+const _server = await createManicServer({ html: app });
 
 // ~manic-touch: 1776275887608
