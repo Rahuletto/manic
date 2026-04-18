0edc3e62569549f60fd5ccbac1a5bd5f7f9346a4
Rahul Marban <rahulmarban@gmail.com>
2026-04-18 09:43:53 +0530

    feat: seo plugin and better seo handling

--- demo/manic.config.ts
+++ demo/manic.config.ts
@@ -1,18 +1,25 @@
 import { defineConfig } from 'manicjs/config';
 import { cloudflare, vercel } from '@manicjs/providers';
 import { apiDocs } from '@manicjs/api-docs';
+import { seo } from '@manicjs/seo';
+import { sitemap } from '@manicjs/sitemap';
+import { mcp } from '@manicjs/mcp';
 
 export default defineConfig({
   app: {
     name: 'Manic',
   },
-  // mode: "frontend",
 
   server: {
     port: 6070,
   },
 
-  plugins: [apiDocs()],
+  plugins: [
+    apiDocs(),
+    seo({ hostname: 'https://manic.js.org' }),
+    sitemap({ hostname: 'https://manic.js.org' }),
+    mcp({ name: 'manic-demo' }),
+  ],
 
   providers: [vercel(), cloudflare()],
 });
